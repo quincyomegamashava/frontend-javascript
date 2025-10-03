98529ac4dbb49cfc722085adf7b5de5f842eeb09
QuincyMashava <mashavaquincy@gmail.com>
2025-10-03 07:40:59 +0200

    frontend js repo

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,5 +1,7 @@
 // task_4/js/main.ts
 
+/// <reference path="./subjects/Teacher.ts" />
+/// <reference path="./subjects/Subject.ts" />
 /// <reference path="./subjects/Cpp.ts" />
 /// <reference path="./subjects/Java.ts" />
 /// <reference path="./subjects/React.ts" />
@@ -11,8 +13,8 @@ export const react = new Subjects.React();
 
 // Create and export one Teacher object cTeacher with experienceTeachingC = 10
 export const cTeacher: Subjects.Teacher = {
-  firstName: 'Guillaume',
-  lastName: 'Salva',
+  firstName: 'John',
+  lastName: 'Doe',
   experienceTeachingC: 10,
 };
 
@@ -23,13 +25,13 @@ console.log(cpp.getRequirements()); // Call getRequirements
 console.log(cpp.getAvailableTeacher()); // Call getAvailableTeacher
 
 // For Java subject
-console.log('\nJava');
+console.log('Java');
 java.setTeacher(cTeacher); // Set cTeacher as the teacher for Java
 console.log(java.getRequirements()); // Call getRequirements
 console.log(java.getAvailableTeacher()); // Call getAvailableTeacher
 
 // For React subject
-console.log('\nReact');
+console.log('React');
 react.setTeacher(cTeacher); // Set cTeacher as the teacher for React
 console.log(react.getRequirements()); // Call getRequirements
 console.log(react.getAvailableTeacher()); // Call getAvailableTeacher
